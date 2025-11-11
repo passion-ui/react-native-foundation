99abc1510400ccc3ae8b994deefdbf21781e9507
Dũng (Wem) <huynh.developer@gmail.com>
2025-11-11 17:40:58 +0700

    chore: update

--- packages/foundation/src/Adapters/VectorIcons.tsx
+++ packages/foundation/src/Adapters/VectorIcons.tsx
@@ -3,7 +3,24 @@ let VectorIcons: any;
 try {
   VectorIcons = require('@expo/vector-icons');
 } catch (e) {
-  VectorIcons = require('react-native-vector-icons').default;
+  VectorIcons = {
+    MaterialCommunityIcons:
+      require('react-native-vector-icons/MaterialCommunityIcons').default,
+    FontAwesome: require('react-native-vector-icons/FontAwesome').default,
+    FontAwesome5: require('react-native-vector-icons/FontAwesome5').default,
+    Ionicons: require('react-native-vector-icons/Ionicons').default,
+    Feather: require('react-native-vector-icons/Feather').default,
+    MaterialIcons: require('react-native-vector-icons/MaterialIcons').default,
+    Entypo: require('react-native-vector-icons/Entypo').default,
+    AntDesign: require('react-native-vector-icons/AntDesign').default,
+    EvilIcons: require('react-native-vector-icons/EvilIcons').default,
+    FontAwesome6: require('react-native-vector-icons/FontAwesome6').default,
+    Foundation: require('react-native-vector-icons/Foundation').default,
+    Octicons: require('react-native-vector-icons/Octicons').default,
+    SimpleLineIcons: require('react-native-vector-icons/SimpleLineIcons')
+      .default,
+    Zocial: require('react-native-vector-icons/Zocial').default,
+  };
 }
 
 export { VectorIcons };
